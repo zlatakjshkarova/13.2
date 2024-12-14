3da51eb3e911ec3bb254ecddc04d90db380b6049
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-12-14 14:55:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+let book = ['Comics', 'history', 'literature', 'mumu', 'aftertheball'];
+console.log(book);
+book.push('FILIPOK', 'Frenchlesson');
+console.log(book);
+book.shift(5);
+console.log(book);
+
+book.forEach(function (element) {
+    console.log(`"Я читаю "  ${element}`);
+})
+
+let book2 = book.indexOf(element)
+
+console.log(book2);
+
+let arr = [1, 2, 3, 4, 5];
+let = arr2 = arr.map(function (element) {
+    return element * 2;
+});
+console.log(arr2);
+let res = book.sort(function (element) {
+    return
+})
+
+
+
+
+//Создать массив содержащий только книги названия  filler
+let books = ['Война и мир', 'Лада', 'Чеk'];
+let newBOO = books.filter((el))=> {
+    return
+}
+
+
+
+//13 прошлое задание//
+console.log("-----13-----");
+let array13 = [10, 8, 5, 1, 8];
+let index;
+for (let i = 0; i < array13.length; i++) {
+    if (array13[i] === 3) {
+        index = `'Число 3 находится под индексом'${i}`;
+
+        break;
+    } else {
+        index = 'В массиве нет такого значения'
+    }
+}
+console.log(index);
+index = array13.indexOf(3);
+if (index == -1) {
+    console.log('Такого элемента нет в массиве')
+} else {
+    index = ''
+}
+console.log(index);
+//этот массив для нохождения одного элемента includes()
+console.log('-----12-----')
+let numbs = [12, 10, 50, 80,];
+let max = numbs[0];
+for (let element of numbs) {
+    if (element > max) {
+        max = element;
+    }
+}
+console.log(max);
+
+
+
+
